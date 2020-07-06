b1d034c0a80b5783189555af7f2aadab9d3274ff
RajniNepali <rajninepali0694@gmail.com>
2020-07-06 17:48:59 +0530

    Commit

--- WebdriverIOReusableFramwork-master/Wrappers/randomEmailGeneratorPage.js
+++
@@ -1,5 +0,0 @@
-//This function generates the random email 
-export function generateRandomEmailAddress() {
-    let emailAddress = "WebIOFramework_" + Math.random().toString().replace('0.', '') + "@webdriverNitor.com";
-    return emailAddress;
-}
